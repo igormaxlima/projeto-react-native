3c3c726a5feb23acd16cf4fb4da88fdb68ad9481
Igor Max de Lima Nunes <122616748+igormaxlima@users.noreply.github.com>
2023-04-15 18:27:46 -0300

    init commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold 
+} from '@expo-google-fonts/montserrat';
+
+      
+import Cesta from './src/telas/Cesta';
+
+export default function App() {
+  const [fontCarregada] = useFonts({
+    "MontSerrratRegular": Montserrat_400Regular,
+    "MontSerratBold": Montserrat_700Bold,
+  });
+  if(!fontCarregada) {
+    return < View/>
+  }
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
+  );
+}
