5220b69c4a1c51b84e603634181eda94d98f0ac3
igormaxlima <igormaxnunes@gmail.com>
2023-04-17 15:23:32 -0300

    upload do mocks

---
+++ src/mocks/cesta.js
@@ -0,0 +1,16 @@
+import logo from '../../assets/assets/logo.png';
+
+const cesta = {
+    topo: {
+        titulo: "Detalhes da Cesta",
+    },
+    detalhes: {
+        nome: "Cesta de Verduras",
+        logoFazenda: logo,
+        nomeFazenda: "Fazenda do Mangs",
+        descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+        preco: "R$ 40,00",
+    },
+}
+
+export default cesta;
