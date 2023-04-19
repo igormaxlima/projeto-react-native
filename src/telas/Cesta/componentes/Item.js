9b542f29cdd42c8c361da16403becca47fdef832
igormaxlima <igormaxnunes@gmail.com>
2023-04-19 15:21:05 -0300

    modificando itens utilizando FlatList

---
+++ src/telas/Cesta/componentes/Item.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+import Texto from "../../../componentes/Texto";
+
+
+export default function Item({ item: { nome, imagem } }) {
+
+    return <View style={estilos.item}>
+    <Image source={imagem} style={estilos.imagem} />
+    <Texto style={estilos.nome}>{ nome }</Texto>
+  </View>
+
+}
+
+const estilos = StyleSheet.create({
+    item : {
+      flexDirection: "row",
+      borderBottomWidth: 1,
+      borderBottomColor: "#ECECEC",
+      paddingVertical: 16,
+      alignItems: "center",
+      paddingVertical: 16,
+      marginHorizontal: 16,
+    },
+    imagem: {
+      width: 60,
+      height: 60,
+    },
+    nome: {
+      fontSize: 16,
+      lineHeight: 26,
+      marginLeft: 11,
+      color: "#464646",
+    }
+      
+
+
+
+})
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,24 +1,39 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, View } from "react-native";
+import { ScrollView, StyleSheet, SafeAreaView, View, FlatList} from "react-native";
 
 import Topo from './componentes/Topo';
 import Detalhes from "./componentes/Detalhes";
-import Itens from "./componentes/Itens";
+import Item from "./componentes/Item";
+import Texto from "../../componentes/Texto";
 
 export default function Cesta({topo, detalhes, itens}) {
-    return <SafeAreaView>
-        <Topo {...topo} />
-
-        <View style={estilos.cesta}>
-            <Detalhes {...detalhes} />
-        </View>
-       <View >
-        <Itens {...itens} />
-        </View>
-    </SafeAreaView>
+    return <>
+        <FlatList
+          data={ itens.lista }
+          renderItem={Item}
+          keyExtractor={({nome}) => nome}
+          ListHeaderComponent={ () => {
+            return <>
+              <Topo {...topo} />
+               <View style={estilos.cesta}>
+                   <Detalhes {...detalhes} />
+                   <Texto style={estilos.titulo}> { itens.titulo } </Texto>
+                </View>
+            </>
+          }}
+        />
+    </>
 }
 
 const estilos = StyleSheet.create({
+    titulo: {
+        color: "#464646",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 23,
+        fontWeight: "bold",
+        lineHeight: 32,
+      },
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16,
